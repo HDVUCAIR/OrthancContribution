bf7fdba31279c85756b0cfa51309d080ab005db6
John Roberts <roberts@ucair.med.utah.edu>
2025-05-08 18:14:51 -0600

    Rearranged backup order and introduced code to NOT acquire a weekly or daily when a monthly is acquired.

--- anon_files/backup_philookup.sh
+++ anon_files/backup_philookup.sh
@@ -8,18 +8,22 @@ dirOut=/backup/
 if [ ! -d "$dirOut" ]; then
    mkdir -p $dirOut
 fi
-fileOut=$dirOut"full_d"$dayOfWeek".sql.gz"
-pg_dumpall | gzip > $fileOut
-
-if [ "$dayOfWeek" = "1" ]; then
-   weekOfMonth=$((($(date +%-d)-1)/7+1))
-   fileOut=$dirOut"full_w"$weekOfMonth".sql.gz"
-   pg_dumpall | gzip > $fileOut
-fi
 
 if [ "$dayOfMonth" = "01" ]; then
    monthOfYear=`date +"%m"`
    fileOut=$dirOut"full_m"$monthOfYear".sql.gz"
    pg_dumpall | gzip > $fileOut
+   exit 0
 fi
+
+if [ "$dayOfWeek" = "1" ]; then
+   weekOfMonth=$((($(date +%-d)-1)/7+1))
+   fileOut=$dirOut"full_w"$weekOfMonth".sql.gz"
+   pg_dumpall | gzip > $fileOut
+   exit 0
+fi
+
+fileOut=$dirOut"full_d"$dayOfWeek".sql.gz"
+pg_dumpall | gzip > $fileOut
+
 
--- phi_files/backup_philookup.sh
+++ phi_files/backup_philookup.sh
@@ -10,18 +10,6 @@ dirOut=/backup/
 if [ ! -d "$dirOut" ]; then
    mkdir -p $dirOut
 fi
-fileOut=$dirOut"philookup_d"$dayOfWeek".tar"
-pg_dump --format=tar -f $fileOut philookup
-fileOut=$dirOut"full_d"$dayOfWeek".sql.gz"
-pg_dumpall | gzip > $fileOut
-
-if [ "$dayOfWeek" = "1" ]; then
-   weekOfMonth=$((($(date +%-d)-1)/7+1))
-   fileOut=$dirOut"philookup_w"$weekOfMonth".tar"
-   pg_dump --format=tar -f $fileOut philookup
-   fileOut=$dirOut"full_w"$weekOfMonth".sql.gz"
-   pg_dumpall | gzip > $fileOut
-fi
 
 if [ "$dayOfMonth" = "01" ]; then
    monthOfYear=`date +"%m"`
@@ -29,5 +17,21 @@ if [ "$dayOfMonth" = "01" ]; then
    pg_dump --format=tar -f $fileOut philookup
    fileOut=$dirOut"full_m"$monthOfYear".sql.gz"
    pg_dumpall | gzip > $fileOut
+   exit 0
 fi
+
+if [ "$dayOfWeek" = "1" ]; then
+   weekOfMonth=$((($(date +%-d)-1)/7+1))
+   fileOut=$dirOut"philookup_w"$weekOfMonth".tar"
+   pg_dump --format=tar -f $fileOut philookup
+   fileOut=$dirOut"full_w"$weekOfMonth".sql.gz"
+   pg_dumpall | gzip > $fileOut
+   exit 0
+fi
+
+fileOut=$dirOut"philookup_d"$dayOfWeek".tar"
+pg_dump --format=tar -f $fileOut philookup
+fileOut=$dirOut"full_d"$dayOfWeek".sql.gz"
+pg_dumpall | gzip > $fileOut
+
 
